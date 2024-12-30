2d20562f8f22e480c9c4ae07015e0f68ea41e123
Alek <92643775+barealek@users.noreply.github.com>
2024-12-30 22:01:56 +0100

    checksum downloading using sha256 hash

--- storage/http.go
+++ storage/http.go
@@ -2,15 +2,18 @@ package storage
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type httpStorage struct {
 	cl       *http.Client
 	endpoint *url.URL
+	// checksums is a map that points any server id to the sum
+	checksums map[string]string
+	mutex     *sync.Mutex
 }
 
 func NewHTTPStorage(endpoint string) (StorageProvider, error) {
@@ -20,14 +23,31 @@ func NewHTTPStorage(endpoint string) (StorageProvider, error) {
 	}
 
 	return &httpStorage{
-		cl:       &http.Client{},
-		endpoint: ep,
+		cl:        &http.Client{},
+		endpoint:  ep,
+		checksums: make(map[string]string),
+		mutex:     &sync.Mutex{},
 	}, nil
 }
 
 // Must return
 func (hs *httpStorage) Store(id string, src io.Reader) error {
-	ep := hs.endpoint.JoinPath(id)
+	checksum, err := hs.getChecksum(id)
+	if err != nil {
+		return err
+	}
+
+	hs.mutex.Lock()
+	existingChecksum, ok := hs.checksums[id]
+	hs.checksums[id] = checksum
+	hs.mutex.Unlock()
+	if ok {
+		if checksum == existingChecksum {
+			return nil
+		}
+	}
+
+	ep := hs.endpoint.JoinPath("data", id)
 	r, err := http.NewRequest("PUT", "", src)
 	r.URL = ep
 	if err != nil {
@@ -38,19 +58,17 @@ func (hs *httpStorage) Store(id string, src io.Reader) error {
 	if err != nil {
 		return err
 	}
-	cnt, _ := io.ReadAll(res.Body)
 	defer res.Body.Close()
-	fmt.Printf("r.URL: %v\n", r.URL)
-	fmt.Printf("cnt: %v\n", string(cnt))
 
 	if res.StatusCode == 200 {
 		return nil
 	}
+
 	return errors.New("non-200 response from http storage provider")
 }
 
 func (hs *httpStorage) Retrieve(id string, dst io.Writer) error {
-	ep := hs.endpoint.JoinPath(id)
+	ep := hs.endpoint.JoinPath("data", id)
 	r, err := http.NewRequest("GET", "", nil)
 	r.URL = ep
 
@@ -70,4 +88,26 @@ func (hs *httpStorage) Retrieve(id string, dst io.Writer) error {
 
 	_, err = io.Copy(dst, res.Body)
 	return err
+}
+
+func (hs *httpStorage) getChecksum(id string) (string, error) {
+	ep := hs.endpoint.JoinPath("checksum", id)
+	r, err := http.NewRequest("GET", "", nil)
+	r.URL = ep
+
+	if err != nil {
+		return "", err
+	}
+
+	res, err := hs.cl.Do(r)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	checksum, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(checksum), nil
 }
